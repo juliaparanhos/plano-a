1f5567598a861c24e55069e37b0116d851bfc191
Júlia Paranhos <julyasantanap@gmail.com>
2020-05-08 22:41:11 -0300

    Updates

---
+++ precache-manifest.b256de69b520c775a64c6954e81c7450.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5573153547faddda0fd60ac0570e5bd",
+    "url": "/plano-a/index.html"
+  },
+  {
+    "revision": "b1a6dc8e65fa8ea83d17",
+    "url": "/plano-a/static/css/2.e20586d2.chunk.css"
+  },
+  {
+    "revision": "f3d34ec7d5abf36f393e",
+    "url": "/plano-a/static/css/main.f52abeb8.chunk.css"
+  },
+  {
+    "revision": "b1a6dc8e65fa8ea83d17",
+    "url": "/plano-a/static/js/2.7ee88d40.chunk.js"
+  },
+  {
+    "revision": "f3d34ec7d5abf36f393e",
+    "url": "/plano-a/static/js/main.aa07c73c.chunk.js"
+  },
+  {
+    "revision": "88aa22ad43f5d8536a3e",
+    "url": "/plano-a/static/js/runtime~main.37e9c892.js"
+  },
+  {
+    "revision": "5f86f814273d051a557ebb0938aeb066",
+    "url": "/plano-a/static/media/background_financial.5f86f814.svg"
+  },
+  {
+    "revision": "aa0ec6bc76b9cabdd6761815fa0845c5",
+    "url": "/plano-a/static/media/banner.aa0ec6bc.jpg"
+  },
+  {
+    "revision": "980fa4f4193229757fd6ff9a4e049304",
+    "url": "/plano-a/static/media/favicon-planoa.980fa4f4.png"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/plano-a/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/plano-a/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/plano-a/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/plano-a/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "3b027909f224cec4ff1833959469fe5f",
+    "url": "/plano-a/static/media/planoa.3b027909.png"
+  }
+]);
